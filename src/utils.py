167a60a8cafc72da4ad362a93fc81f9faabc21da
Datarchpy <datarch.py2011@gmail.com>
2024-07-01 17:48:47 +0900

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,12 @@
+"""
+共通で使用するユーティリティ関数を定義します。
+機能:
+共通処理の関数化
+"""
+def format_message(message):
+    # メッセージのフォーマットを行うユーティリティ関数
+    return f"User: {message['user']} - {message['text']}"
 
+def calculate_reactions(reactions):
+    # リアクションの数を計算するユーティリティ関数
+    return sum(reaction['count'] for reaction in reactions)
